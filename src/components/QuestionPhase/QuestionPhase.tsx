af25b1351a81b93cc6a25193be177daf56fa7be0
VadimOMEGA <vadimivanov.0320@gmail.com>
2025-06-20 15:38:22 +0300

    Latest commit

--- src/components/QuestionPhase/QuestionPhase.tsx
+++ src/components/QuestionPhase/QuestionPhase.tsx
@@ -75,7 +75,7 @@ const QuestionPhase = ({ role }: IRoleRevealProps) => {
 			}}
 		>
 			{!showQuestion && (
-				<div className='w-screen min-h-screen flex items-center justify-center pt-2 pb-6'>
+				<div className='w-screen min-h-screen flex items-center justify-center'>
 					<motion.div
 						layoutId='game-card'
 						transition={{
@@ -95,7 +95,7 @@ const QuestionPhase = ({ role }: IRoleRevealProps) => {
 			)}
 
 			{showQuestion && (
-				<div className='w-screen min-h-screen flex lg:flex-row flex-col-reverse items-center justify-center lg:gap-[8.5rem] gap-6 px-1.5 pt-2 pb-6'>
+				<div className='w-screen min-h-screen flex lg:flex-row flex-col-reverse items-center justify-center lg:gap-[8.5rem] gap-6 px-1.5'>
 					<motion.div
 						layoutId='game-card'
 						transition={{
